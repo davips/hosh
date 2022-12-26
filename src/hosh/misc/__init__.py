01d28d63e42ae9e8f26bd106a51198de91813616
davips <dpsabc@gmail.com>
2022-12-26 16:56:24 -0300

    Add helper iterators for list of hoshed objects

--- src/hosh/misc/__init__.py
+++ src/hosh/misc/__init__.py
@@ -20,3 +20,36 @@
 #  part of this work is illegal and unethical regarding the effort and
 #  time spent here.
 """Support modules are hidden here to avoid polluting the API"""
+
+
+def hoshes(iterable):
+    """
+    Iterator mapping a list of objects to their hoshes.
+
+    >>> from collections import OrderedDict as D
+    >>> from hosh import Hosh
+    >>> def f(o):
+    ...     o.hosh = Hosh(o["a"].encode())
+    ...     return o
+    >>> lst = map(f, [D(a="a"), D(a="b"), D(a="c")])
+    >>> [h.id for h in hoshes(lst)]
+    ['cIXBKPediDiOKabeZ6SthD04rnzaquNXaAEhSud4', '9a.VuUhND-A6QgCpIs7lAAkQnQuXwplmiw8308ao', 'SRbk-ZIvvxEJtxP6dHbZP-gQeBGTGCWI8-d3WK3n']
+    """
+    from operator import attrgetter
+    return map(attrgetter("hosh"), iterable)
+
+def ids(iterable):
+    """
+    Iterator mapping a list of objects to their hosh ids.
+
+    >>> from collections import OrderedDict as D
+    >>> from hosh import Hosh
+    >>> def f(o):
+    ...     o.hosh = Hosh(o["a"].encode())
+    ...     return o
+    >>> lst = map(f, [D(a="a"), D(a="b"), D(a="c")])
+    >>> [h for h in ids(lst)]
+    ['cIXBKPediDiOKabeZ6SthD04rnzaquNXaAEhSud4', '9a.VuUhND-A6QgCpIs7lAAkQnQuXwplmiw8308ao', 'SRbk-ZIvvxEJtxP6dHbZP-gQeBGTGCWI8-d3WK3n']
+    """
+    from operator import attrgetter
+    return map(attrgetter("hosh.id"), iterable)
